b2ebf9a72b61a3c21de25051351011b9adf72c82
Anass Fajoui <anasslpro165@gmail.com>
2025-02-05 14:05:44 +0100

    adding the accuracy feature

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 let input = document.querySelector("input");
 let speedSpan = document.querySelector(".speed");
+let accuracySpan = document.querySelector(".accuracy");
 let countSpan = document.querySelector(".counter");
 let restart = document.querySelector(".restart");
 
 
 let started = false;
 
-let text = "The quick brown fox jumps over the lazy dog. Speed and accuracy are key when typing fast. Stay focused, avoid errors, and keep your fingers moving smoothly. Practice makes perfect!";
+// let text = "The quick brown fox jumps over the lazy dog. Speed and accuracy are key when typing fast. Stay focused, avoid errors, and keep your fingers moving smoothly. Practice makes perfect!";
+let text= "anass basma jannet, anass basma jannet, anass basma jannet";
 
 window.onload = function(){
     input.focus();
@@ -19,24 +21,63 @@ restart.onclick = function(){
     input.style.opacity = "1";
     input.focus();
     started = false;
-    countSpan.textContent = "30";
+    countSpan.textContent = "10";
     speedSpan.textContent = "__";
+    accuracySpan.textContent = "__";
+    cursor = 0;
+    correct = 0;
+    lastFlag = true;
 }
+let cursor = 0;
+let correct = 0;
+let char;
+let lastFlag = true
 
-input.oninput = function(){
+input.oninput = function(event){
     if (!started){
         started = true;
+        char = input.value;
+        if (char === text[cursor]){
+            correct++;
+            cursor++;
+            lastFlag = true;
+            console.log(char)
+        }else{
+            cursor++;
+            lastFlag = false;
+        }
         interval = setInterval(function () {
             countSpan.textContent--;
             if (countSpan.textContent == 0){
                 clearInterval(interval);
-                let speed = (input.value.length / 5) * 2;
+                let speed = (correct / 5) * 6;
+                let accuracy = (correct / input.value.length) * 100;
                 input.blur();
                 input.style.pointerEvents = "none";
                 input.style.opacity = "0.7";
                 speedSpan.textContent = `${speed.toFixed(0)}`;
+                accuracySpan.textContent = `${accuracy.toFixed(0)}`
+                console.log(`Correct character are : ${correct}`)
             }
         }, 1000)
+    }else {
+        if (event.inputType === "deleteContentBackward"){
+            if (lastFlag){
+                correct--;
+            }
+            cursor--;
+        }else{
+            char = input.value.slice(-1);
+            if (char === text[cursor]){
+                cursor++;
+                correct++;
+                console.log(char);
+                lastFlag = true;
+            }else{
+                cursor++;
+                lastFlag = false;
+            }
+        }
     }
 }
 
