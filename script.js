07cadd9dc941611aa4169975403b04f9d95be854
Anass Fajoui <anasslpro165@gmail.com>
2025-02-05 02:35:43 +0100

    very rudimentary Speed Test

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let input = document.querySelector("input");
 let speedSpan = document.querySelector(".speed");
 let countSpan = document.querySelector(".counter");
+let restart = document.querySelector(".restart");
+
 
 let started = false;
 
@@ -9,12 +11,22 @@ let text = "The quick brown fox jumps over the lazy dog. Speed and accuracy are 
 window.onload = function(){
     input.focus();
 }
+let interval;
+restart.onclick = function(){
+    clearInterval(interval);
+    input.value = "";
+    input.style.pointerEvents = "auto";
+    input.style.opacity = "1";
+    input.focus();
+    started = false;
+    countSpan.textContent = "30";
+    speedSpan.textContent = "__";
+}
 
 input.oninput = function(){
     if (!started){
         started = true;
-        console.log("counter starts here :")
-        let interval = setInterval(function () {
+        interval = setInterval(function () {
             countSpan.textContent--;
             if (countSpan.textContent == 0){
                 clearInterval(interval);
